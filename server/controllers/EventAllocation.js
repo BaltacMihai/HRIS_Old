d6a527fb0f66c61d61001c10f762582136dc0db8
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-15 12:43:30 +0200

    get event infos by userId and interval

--- server/controllers/EventAllocation.js
+++ server/controllers/EventAllocation.js
@@ -1,5 +1,52 @@
 const EventAllocationDB = require("./../models").EventAllocation;
+const EventDB = require("./../models").Event;
+const ProjectDB = require("../models").Project;
 
-const controller = {};
+const controller = {
+  findEventsByIntervalAndUser: async (req, res) => {
+    const { Op } = require("@sequelize/core");
+    const { userId, startingDate, endingDate } = req.params;
+
+    // if (startingDate < endingDate) {
+    //   res
+    //     .status(400)
+    //     .send({ message: "Incorrect request", startingDate, endingDate });
+    // }
+
+    if (userId < 0) {
+      res.status(400).send({ message: "User doesn't exist" });
+    }
+
+    EventAllocationDB.findAll({
+      where: {
+        userId: userId,
+      },
+
+      include: [
+        {
+          model: EventDB,
+          attributes: ["type", "name", "endingDate"],
+          where: {
+            endingDate: {
+              [Op.gt]: startingDate,
+            },
+            endingDate: {
+              [Op.lt]: endingDate,
+            },
+          },
+          include: [{ model: ProjectDB, attributes: ["color"] }],
+        },
+      ],
+      attributes: ["eventId"],
+    })
+      .then((user) => {
+        res.status(200).send(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send({ message: "Server error" });
+      });
+  },
+};
 
 module.exports = controller;
--- server/routes/EventAllocation.js
+++ server/routes/EventAllocation.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 const eventAllocationController = require("./../controllers").EventAllocation;
 
+//http://localhost:3031/api/events-allocation/1/2022-03-15/2022-03-18
+router.get(
+  "/:userId/:startingDate/:endingDate",
+  eventAllocationController.findEventsByIntervalAndUser
+);
+
 module.exports = router;
