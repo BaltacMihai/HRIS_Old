6a2b422688ed99754639b7e4d6cb0651236599b4
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-16 16:04:13 +0200

    my project widget connected with backend

--- client/src/components/Widgets/MyProjects.jsx
+++ client/src/components/Widgets/MyProjects.jsx
@@ -1,35 +1,42 @@
 import React from "react";
+import useUsersProjects from "../../hooks/findUsersProjects";
 import ProjectCard from "../ProjectCard";
 import TaskCard from "../TaskCard";
 
-function MyProjects() {
-  let events = [
-    {
-      name: "Create the Dashbord Design",
-      role: "Developer",
-    },
-    {
-      name: "Create the Dashbord Design",
-      role: "Developer",
-    },
-    {
-      name: "Create the Dashbord Design",
-      role: "Developer",
-    },
-    {
-      name: "Create the Dashbord Design",
-      role: "Developer",
-    },
-  ];
+function MyProjects({ id }) {
+  let event = useUsersProjects(id);
+  let events = null;
 
-  return (
-    <div className="widget myProjects">
-      <div className="myProjects_header">
-        <p className="myProjects_header_title">My Projects</p>
+  if (event && events == null) {
+    events = event.map((e) => {
+      return {
+        id: e.Project.id,
+        name: e.Project.name,
+
+        color: e.Project.color,
+        role: e.type,
+      };
+    });
+  }
+
+  if (events)
+    return (
+      <div className="widget myProjects">
+        <div className="myProjects_header">
+          <p className="myProjects_header_title">My Projects</p>
+        </div>
+        <div className="myProjects_container">{mapTheTasks(events)}</div>
       </div>
-      <div className="myProjects_container">{mapTheTasks(events)}</div>
-    </div>
-  );
+    );
+  else {
+    return (
+      <div className="widget myProjects">
+        <div className="myProjects_header">
+          <p className="myProjects_header_title">My Projects</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapTheTasks(tasks) {
--- server/routes/ProjectAllocation.js
+++ server/routes/ProjectAllocation.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const projectAllocationController =
   require("./../controllers").ProjectAllocation;
 
-//http://localhost:3031/api/project-allocation/user/1
 router.get("/user/:userId", projectAllocationController.findUsersProjects);
 
 module.exports = router;
