e3e9062478fc33e0adfb26024ad658e15c5cd39f
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-16 15:58:32 +0200

    backend route created for the user's projects

--- server/controllers/ProjectAllocation.js
+++ server/controllers/ProjectAllocation.js
@@ -1,5 +1,32 @@
 const ProjectAllocationDB = require("./../models").ProjectAllocation;
+const ProjectDB = require("../models").Project;
 
-const controller = {};
+const controller = {
+  findUsersProjects: async (req, res) => {
+    const { Op } = require("@sequelize/core");
+    const { userId } = req.params;
+
+    if (userId < 0) {
+      res.status(400).send({ message: "User doesn't exist" });
+    }
+
+    ProjectAllocationDB.findAll({
+      where: {
+        userId: userId,
+      },
+
+      include: [{ model: ProjectDB, attributes: ["id", "color", "name"] }],
+
+      attributes: ["type"],
+    })
+      .then((event) => {
+        res.status(200).send(event);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send({ message: "Server error" });
+      });
+  },
+};
 
 module.exports = controller;
