72c36d792cd1e0f13baae9d1bccfed75f0dbe630
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-17 13:42:31 +0200

    unharcoded enteties

--- client/src/components/TasksTable.jsx
+++ client/src/components/TasksTable.jsx
@@ -1,237 +1,94 @@
 import React from "react";
 
-function TasksTable() {
+function TasksTable({ id }) {
+  let tasks = [
+    {
+      project: "DTS",
+      name: "Do to something very important",
+      status: "Pending",
+      beggining: "17/03/2022",
+      ending: "19/03/2022",
+    },
+    {
+      project: "DTS",
+      name: "Do to something very important",
+      status: "Pending",
+      beggining: "17/03/2022",
+      ending: "19/03/2022",
+    },
+    {
+      project: "DTS",
+      name: "Do to something very important",
+      status: "Pending",
+      beggining: "17/03/2022",
+      ending: "19/03/2022",
+    },
+    {
+      project: "DTS",
+      name: "Do to something very important",
+      status: "Pending",
+      beggining: "17/03/2022",
+      ending: "19/03/2022",
+    },
+    {
+      project: "DTS",
+      name: "Do to something very important",
+      status: "Pending",
+      beggining: "17/03/2022",
+      ending: "19/03/2022",
+    },
+  ];
+
   return (
     <div className="task_table">
-      <div className="task_table_header">
-        <p className="task_table_header_item">Project</p>
-        <p className="task_table_header_item">Task</p>
-        <p className="task_table_header_item">Progress</p>
-        <p className="task_table_header_item">Start task</p>
-        <p className="task_table_header_item">Deadline</p>
-        <p className="task_table_header_item">Actions</p>
-      </div>
-
-      <div className="task_table_content">
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>{" "}
-        <div className="task_table_content_entity">
-          <p className="task_table_content_entity_info">Project Number 1</p>
-          <p className="task_table_content_entity_info">Task no. 1</p>
-          <p className="task_table_content_entity_info">--------____</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">03/01/2021</p>
-          <p className="task_table_content_entity_info">...</p>
-        </div>
-      </div>
+      {returnTableHeader()}
+      {returnTableContent(tasks)}
     </div>
   );
 }
 
+function returnTableHeader() {
+  return (
+    <div className="task_table_header">
+      <p className="task_table_header_item">Project</p>
+      <p className="task_table_header_item">Task</p>
+      <p className="task_table_header_item">Progress</p>
+      <p className="task_table_header_item">Start task</p>
+      <p className="task_table_header_item">Deadline</p>
+      <p className="task_table_header_item">Actions</p>
+    </div>
+  );
+}
+
+function returnTableContent(tasks) {
+  console.log(tasks);
+  return (
+    <div className="task_table_content">
+      {tasks.map((e) => {
+        return (
+          <Entity
+            project={e.project}
+            name={e.name}
+            status={e.status}
+            beggining={e.beggining}
+            ending={e.ending}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Entity({ project, name, status, beggining, ending }) {
+  return (
+    <div className="task_table_content_entity">
+      <p className="task_table_content_entity_info">{project}</p>
+      <p className="task_table_content_entity_info">{name}</p>
+      <p className="task_table_content_entity_info">{status}</p>
+      <p className="task_table_content_entity_info">{beggining}</p>
+      <p className="task_table_content_entity_info">{ending}</p>
+      <p className="task_table_content_entity_info">...</p>
+    </div>
+  );
+}
 export default TasksTable;
