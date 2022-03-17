65a8e9383f3ef9bdcba50e5f1759fe08bbf3f626
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-17 12:40:49 +0200

    task page created

---
+++ client/src/components/TasksTable.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+
+function TasksTable() {
+  return (
+    <div className="task_table">
+      <div className="task_table_header">
+        <p className="task_table_header_item">Project</p>
+        <p className="task_table_header_item">Task</p>
+        <p className="task_table_header_item">Progress</p>
+        <p className="task_table_header_item">Start task</p>
+        <p className="task_table_header_item">Deadline</p>
+        <p className="task_table_header_item">Actions</p>
+      </div>
+
+      <div className="task_table_content">
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>{" "}
+        <div className="task_table_content_entity">
+          <p className="task_table_content_entity_info">Project Number 1</p>
+          <p className="task_table_content_entity_info">Task no. 1</p>
+          <p className="task_table_content_entity_info">--------____</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">03/01/2021</p>
+          <p className="task_table_content_entity_info">...</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TasksTable;
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import TasksTable from "../components/TasksTable";
 
 function Tasks() {
   return (
     <div className="page tasks">
       <Navbar current="tasks" />
-      <p>Tasks</p>
+      <div className="tasks_content">
+        {returnActions()}
+        <TasksTable />
+      </div>
+    </div>
+  );
+}
+
+function returnActions() {
+  return (
+    <div className="tasks_content_actions">
+      <div className="tasks_content_actions_filter">
+        <div className="tasks_content_actions_filter_search">
+          <input type="text" />
+          <span className="icon-search icon"></span>
+        </div>
+        <p className="tasks_content_actions_filter_quick">Quick Filters</p>
+      </div>
+      <div className="tasks_content_actions_add">
+        <span className="icon-plus"></span>
+      </div>
     </div>
   );
 }
