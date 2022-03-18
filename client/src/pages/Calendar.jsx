8ec2979af7747d93b2f0f5bd6da2a56982f9c29b
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-03-18 15:37:11 +0200

    calendar header

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-function Calendar() {
+function Calendar({ userId }) {
   return (
     <div className="page calendar">
       <Navbar current="calendar" />
-      <p>Calendar</p>
+      {returnContent(userId)}
+    </div>
+  );
+}
+
+function returnContent(userId) {
+  return <div className="calendar_content">{returnActions()}</div>;
+}
+
+function returnActions() {
+  return (
+    <div className="calendar_content_header">
+      <p className="calendar_content_header_title">My Schedule</p>
+      <div className="calendar_content_header_menu">
+        <div className="calendar_content_header_menu_item calendar_content_header_menu_item-active">
+          Month
+        </div>
+        <div className="calendar_content_header_menu_item">Week</div>
+        <div className="calendar_content_header_menu_item">Day</div>
+      </div>
     </div>
   );
 }
