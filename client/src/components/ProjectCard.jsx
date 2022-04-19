d34cfb3dfa79b3d2286002f13613471dc544b490
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-04-19 16:33:05 +0300

    connected with links

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function ProjectCard({ name, role, id, color }) {
+  let location = "/project/" + id;
   useEffect(() => {
     let projectColor = document.getElementById(name + "-" + id);
     projectColor.style.backgroundColor = color;
   });
   return (
-    <div className="project_card" id={name + "-" + id}>
-      <p className="project_card_title">{name}</p>
-      <p className="project_card_role">Rol: {role}</p>
-    </div>
+    <Link to={location}>
+      <div className="project_card" id={name + "-" + id}>
+        <p className="project_card_title">{name}</p>
+        <p className="project_card_role">Rol: {role}</p>
+      </div>
+    </Link>
   );
 }
 
